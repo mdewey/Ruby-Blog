58288e78740153a4969e646e693587526b059dda
Mark Dewey <mark.t.dewey@gmail.com>
2017-06-17 15:49:36 -0400

    delete works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,31 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
     end
 
     def index
       @articles = Article.all
     end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def destroy
+      @article =  Article.find(params[:id])
+      @article.destroy
+      redirect_to action: "index"
+    end
+    
+
+    def update
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+         redirect_to action: "index"
+       else
+          render "edit"
+       end
+    end
 
     def show
       @article = Article.find(params[:id])
@@ -14,8 +34,11 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+          redirect_to action: "index" 
+        else
+          render 'new'
+        end
     end
 
     private
